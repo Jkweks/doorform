6a129af94c0be5eebb0773ef9ed9f23588e4ff48
Jkweks <143143798+Jkweks@users.noreply.github.com>
2025-08-16 15:25:44 -0400

    feat: provide default door part presets

---
+++ frontend/js/doorPartPresets.js
@@ -0,0 +1,10 @@
+const DOOR_PART_PRESETS = [
+  {
+    id: 'standard',
+    name: 'Standard',
+    topRail: 'TR-001',
+    bottomRail: 'BR-001',
+    hingeRail: 'HR-001',
+    latchRail: 'LR-001'
+  }
+];
